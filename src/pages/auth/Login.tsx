1ff0113c58caf2af353938ef09dda3f9c4eaf60e
stepanenko <mef.yas@gmail.com>
2023-05-01 14:59:19 +0300

    feat: added login page

---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import './Login.css';
+import { Button, Form, Input, Layout, Typography } from 'antd';
+import AnchorLink from 'antd/es/anchor/AnchorLink';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+  let navigate = useNavigate();
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+    setLoading(true);
+    // perform login logic here
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const submitForm = () => {
+    navigate('/');
+  };
+
+  return (
+    <Layout style={{ backgroundColor: '#fff' }} className="login-layout">
+      <Form
+        form={form}
+        name="login-form"
+        initialValues={{
+          remember: true
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}>
+        <Typography.Title level={1} className="login-title">
+          Login
+        </Typography.Title>
+
+        <Form.Item
+          className="login-form-item"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!'
+            }
+          ]}>
+          <Input placeholder="Username" className="login-input" />
+        </Form.Item>
+
+        <Form.Item
+          className="login-form-item"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!'
+            }
+          ]}>
+          <Input.Password type="password" placeholder="Password" className="login-input" />
+        </Form.Item>
+
+        <Form.Item className="login-form-item" name="forgot">
+          <AnchorLink
+            className="login-anchor-forgot-password"
+            title="Forgot your password?"
+            href="#"
+          />
+        </Form.Item>
+
+        <Form.Item className="login-form-item">
+          <Button type="primary" htmlType="submit" loading={loading} onClick={submitForm}>
+            Sign in
+          </Button>
+          {/* <Button type="default" htmlType="button" loading={loading}>
+            Register
+          </Button> */}
+        </Form.Item>
+      </Form>
+    </Layout>
+  );
+}
+
+export default Login;
