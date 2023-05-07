f2fbfeebbd2bbcf6b6d66d68e1497921c52143bd
stepanenko <mef.yas@gmail.com>
2023-05-07 16:27:05 +0300

    feat: added some stuff to Admin's Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,26 +1,58 @@
-import { useEffect, useState } from 'react';
+import Grid from '@mui/material/Unstable_Grid2';
+import './Home.css';
+import { Button, Stack } from '@mui/material';
+// import { useEffect, useState } from 'react';
 
 export function Home() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  // const [loggedIn, setLoggedIn] = useState(false);
 
-  useEffect(() => {
-    (async () => {
-      const data = await fetch('/api/authentication');
-      const isLoggedIn = data.status !== 401;
+  // useEffect(() => {
+  //   (async () => {
+  //     const data = await fetch('/api/authentication');
+  //     const isLoggedIn = data.status !== 401;
 
-      setLoggedIn(isLoggedIn);
-    })();
-  }, []);
+  //     setLoggedIn(isLoggedIn);
+  //   })();
+  // }, []);
 
-  const text = loggedIn ? (
-    <div>
-      <h1>Welcome to Student E-Journal</h1>
-    </div>
-  ) : (
-    <div>
-      <h1>Please log in</h1>
-    </div>
+  // const text = loggedIn ? (
+  //   <div>
+  //     <h1>Welcome to Student E-Journal</h1>
+  //   </div>
+  // ) : (
+  //   <div>
+  //     <h1>Please log in</h1>
+  //   </div>
+  // );
+
+  // return <div>{text}</div>;
+
+  // const [specialities, setSpecialities] = useState([]);
+  // const [specialitiesVisibility, setSpecialitiesVisibility] = useState(false);
+
+  async function handleSpecialitiesOnClick() {
+    const data = await fetch('/api/specialities');
+    const res = await data.json();
+    console.log(res);
+  }
+
+  return (
+    <Grid container spacing={2} className="main-grid">
+      <Grid xs={12} className="top-menu-grid">
+        <div>xs=6 md=4</div>
+      </Grid>
+      <Grid xs={2} className="left-menu-grid">
+        <Stack spacing={2}>
+          <Button variant="text" onClick={handleSpecialitiesOnClick}>
+            Specialities
+          </Button>
+          <Button variant="text">Text</Button>
+          <Button variant="text">Text</Button>
+        </Stack>
+      </Grid>
+      <Grid xs={10} className="main-content-grid">
+        <div>xs=6 md=8</div>
+      </Grid>
+    </Grid>
   );
-
-  return <div>{text}</div>;
 }
